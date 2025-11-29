aa0a1522597579f84b4154be73b925b510f1b925
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-11-29 12:11:27 -0500

    feat: add responsive message layout

--- app/(momento)/layout.tsx
+++ app/(momento)/layout.tsx
@@ -12,18 +12,20 @@ export default function MomentoLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const isMessagesPage = pathname.startsWith("/messages");
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return (
-    <div className="md:w-[90vw] xl:w-[70vw] 2xl:w-[58vw] max-md:w-full mx-auto md:flex">
+    <div className={`${isMessagesPage ? "w-full" : "md:w-[90vw] xl:w-[70vw] 2xl:w-[58vw]"} max-md:w-full mx-auto md:flex`}>
       <Topbar />
       <LeftSidebar />
       <section className="flex flex-1 md:h-screen min-h-[100vh]">
         {children}
       </section>
+      <Bottombar />
     </div>
   );
 }
